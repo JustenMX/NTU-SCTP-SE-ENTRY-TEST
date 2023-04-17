8aac37cad6a0da7f2a0b36e7ab1ea8125b7ce22a
Justen Manni <mannijusten@gmail.com>
2023-04-17 12:51:19 +0800

    Update Hero text in homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,11 +12,11 @@ function HomePage() {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">Hello there!</h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Greetings from Justen Manni. I will be attempting all 6 questions
+            and my answers can be found in each of the questions respective
+            pages.
           </p>
           {/* Question Buttons */}
           <div className="flex-row">
