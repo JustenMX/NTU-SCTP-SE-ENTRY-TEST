0f71abb3d54144c0451aeec7e4714a17cc307c48
Justen Manni <mannijusten@gmail.com>
2023-04-19 04:52:04 +0800

    Update CSS changes to QuestionOne.jsx

--- src/pages/QuestionOne.jsx
+++ src/pages/QuestionOne.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
-import js from "react-syntax-highlighter/dist/esm/languages/hljs/javascript";
 
 function QuestionOne() {
   // Question One Answer
@@ -20,33 +19,41 @@ function QuestionOne() {
     swap(2, 3);
     swap(2, "animal");
     swap(100, 2000);
-  }`;
+  }
+  `;
 
   return (
     <>
-      <div className="dark:text-teal-100">
-        <h1 className="font-bold text-2xl text-center m-10">Question One</h1>
-      </div>
-      <div className="ml-50 dark:text-teal-100">
-        <h3 className="font-semibold text-lg my-2">Task 1</h3>
-        <ul className="list-inside list-disc">
-          <li>
-            Create a function that would swap the value of x and y using only x
-            and y as variables.
-          </li>
-          <li>x and y must be numeric</li>
-          <li>return -1 if x and y is not numeric</li>
-          <li>print the swapped values in the console</li>
-        </ul>
-        <h3 className="font-semibold text-lg mt-4 mb-2">Task 2</h3>
-        <ul className="list-inside list-disc">
-          <li>Invoke the function &quot;swap&quot;</li>
-        </ul>
-      </div>
-      <div>
-        <SyntaxHighlighter language={js} style={docco}>
-          {ansQuestionOne}
-        </SyntaxHighlighter>
+      <div className="max-w-screen-lg px-4 md:px-8 mx-auto flex flex-col h-screen">
+        <div className="dark:text-teal-100">
+          <h1 className="font-bold text-2xl text-center m-10">Question One</h1>
+        </div>
+        <div className="ml-50 dark:text-teal-100">
+          <h3 className="font-semibold text-lg my-2">Task 1</h3>
+          <ul className="list-inside list-disc">
+            <li>
+              Create a function that would swap the value of x and y using only
+              x and y as variables.
+            </li>
+            <li>x and y must be numeric</li>
+            <li>return -1 if x and y is not numeric</li>
+            <li>print the swapped values in the console</li>
+          </ul>
+          <h3 className="font-semibold text-lg mt-4 mb-2">Task 2</h3>
+          <ul className="list-inside list-disc mb-4">
+            <li>Invoke the function &quot;swap&quot;</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="font-semibold text-lg my-2">Solution</h3>
+          <SyntaxHighlighter
+            className="text-lg"
+            language="javascript"
+            style={docco}
+          >
+            {ansQuestionOne}
+          </SyntaxHighlighter>
+        </div>
       </div>
     </>
   );
